3914589063d55dc28f8d4e3354c7459e5714138e
Elephant <sby-kov@mail.ru>
2020-05-08 21:30:18 +0700

    первый commit + Push into GitHub

---
+++ src/FirstServlet.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FirstServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter pw = response.getWriter();
+
+        pw.println("<h1>");
+        pw.println("FirstServlet:  Здравствуй, МИР ! ! ");
+        pw.println("</h1>");
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User:  Marina
+  Дата:  08.05.2020
+  Время: 17:51
+  To change this template use File | Settings | File Templates.
+  language="java"
+--%>
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
