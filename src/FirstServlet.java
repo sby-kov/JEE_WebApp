b78a472f0e0f38522980d3089e8453b7ee92831f
Elephant <sby-kov@mail.ru>
2020-05-11 22:28:05 +0700

    Session Count (почему-то 1,1,2,...)

--- src/FirstServlet.java
+++ src/FirstServlet.java
@@ -1,3 +1,4 @@
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -7,7 +8,17 @@ public class FirstServlet extends javax.servlet.http.HttpServlet {
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        // параметры содержатся в объекте request
+        // сессия. сделаем счетчик
+        HttpSession session = request.getSession();
+        Integer count = (Integer) session.getAttribute("count");
+        if (count == null) {
+            count = 1;
+            session.setAttribute("count", count);
+        } else {
+            session.setAttribute("count", count + 1);
+        }
+
+        // параметры. содержатся в объекте request
         String name = request.getParameter("name");
         String mess;    // сообщение о параметрах
         if (name != null) { mess = "Привет, " + name + " !";
@@ -18,6 +29,7 @@ public class FirstServlet extends javax.servlet.http.HttpServlet {
 
         pw.println("<h1>");
         pw.println("FirstServlet:  Здравствуй, МИР ! ха-ха! ");
+        pw.println("<p> Ваш счетчик: " + count + " </p>");
         pw.println("<p>" + mess + "</p>");
         pw.println("</h1>");
     }
